a8501dace4587769e3105c57f56b08194d6d5dc7
hamza <hamzazizkhan@gmail.com>
2025-02-04 17:03:18 +0300

    added main MMA table and links for spider

--- fighterDB.py
+++ fighterDB.py
@@ -4,14 +4,24 @@ conn = sqlite3.connect('fighters.sqlite')
 cur = conn.cursor()
 
 cur.execute('''
-DROP TABLE IF EXISTS data
+DROP TABLE IF EXISTS profesional_record_data
+''')
+cur.execute('''
+DROP TABLE IF EXISTS MMA_record
 ''')
 
 cur.execute('''
-CREATE TABLE data (firstName TEXT, lastName TEXT, matches INT, wins INT, losses INT, 
+CREATE TABLE profesional_record_data (firstName TEXT, lastName TEXT, matches INT, wins INT, losses INT, 
             knockoutWins INT, knockoutLosses INT, submissionWins INT, submissionLosses INT, 
             decisionWins INT, decisionLosses INT)
 ''')
+cur.execute('''
+CREATE TABLE MMA_record (firstName TEXT, lastName TEXT, result TEXT, record NUMERIC, opponent TEXT, method TEXT, event TEXT,
+            date DATE, round INT, time NUMERIC, location TEXT, notes TEXT)
+''')
+
+########################### prof record
+
 fighters_data = open('fighters.txt', 'r')
 fighters_list = fighters_data.readlines()
 
@@ -20,12 +30,47 @@ for row in fighters_list:
         col = row.split()
         print(col)
 
-        cur.execute('''INSERT INTO data (firstName, lastName, matches, wins, losses, 
+        cur.execute('''INSERT INTO profesional_record_data (firstName, lastName, matches, wins, losses, 
             knockoutWins, knockoutLosses, submissionWins, submissionLosses, 
             decisionWins, decisionLosses) VALUES (?,?,?,?,?,?,?,?,?,?,?)''',
             (col[1],col[3],col[5],col[7],col[9],col[11], col[13], 
              col[15], col[17], col[19], col[21]) )
         conn.commit()
         
+
+########################### main table
+main_table = open('main_Table.txt','r').readlines()
+
+for fighter_line in main_table:
+    elements = fighter_line.split('// ')
+    print(len(elements))
+    fighter_first_name = elements[0].split()[0]
+    fighter_last_name = elements[0].split()[1]
+
+    for i in range(1,len(elements)-1,10):
+        print(i)
+        result = elements[i]
+        record = elements[i+1]
+        opponent = elements[i+2]
+        method = elements[i+3]
+        event = elements[i+4]
+        date = elements[i+5]
+        round = elements[i+6]
+        time = elements[i+7]
+        location = elements[i+8]
+        notes = elements[i+9]
+        print((result, record, opponent, method, event,
+        date, round, time, location, notes))
+        cur.execute('''
+                    INSERT INTO MMA_record (firstName, lastName, result, record, opponent, method, event,
+        date, round, time, location, notes) VALUES (?,?,?,?,?,?,?,?,?,?,?,?)
+                    ''',(fighter_first_name, fighter_last_name, result, record, opponent, method, event,
+        date, round, time, location, notes))
+
+        conn.commit()
+        
+          
+
+fighters_data.close()
 cur.close()
 
---
+++ spider.py
@@ -0,0 +1,69 @@
+import sqlite3
+import requests
+from bs4 import BeautifulSoup
+import os
+
+url = "https://en.wikipedia.org/wiki/Khabib_Nurmagomedov"
+html_content=requests.get(url).text
+#print(html_content)
+soup = BeautifulSoup(html_content, "html.parser")
+
+fighter_name = soup.find('h1').get_text()
+heading_two = soup.find_all('h2')
+
+prof_rec_table = None
+main_rec_table = None
+
+for heading in heading_two:
+    # Mixed martial arts record section
+    if 'id' in heading.attrs:
+        if heading['id']=='Mixed_martial_arts_record':
+            print('found Mixed martial arts record section')
+            print(heading)
+
+            # Professional record breakdown table
+            prof_rec_table = heading.string.next_element.next_element
+            print('found Professional record breakdown table')
+
+            for ele in prof_rec_table.next_elements:
+                if ele.name == 'table':
+                    main_rec_table = ele
+                    print('found main rec table \n')
+                    print(main_rec_table.prettify)
+                    break
+            print('=================')
+
+            break
+
+print('\n')
+print('=================')
+
+if(os.path.exists('main_table.txt')): os.remove('main_table.txt')
+file = open('main_table.txt', 'a')
+file.write(fighter_name.split()[0] + ' ' + fighter_name.split()[1]+ '// ')
+
+rows = main_rec_table.find_all('tr')
+
+qu = []
+i=0
+for row in rows:
+    if i ==0:
+        i=1
+        continue
+    #print(row)
+    td = row.find_all('td')
+    print('=================')
+    print('fighters link!')
+    opponent = td[2]
+    a_tag = opponent.a
+    if a_tag is not None:
+        href = a_tag['href']
+        link = 'https://en.wikipedia.org'+href
+        if link not in qu:
+            qu.append(link)
+    print('=================')
+
+print(qu)
+    
+
+
