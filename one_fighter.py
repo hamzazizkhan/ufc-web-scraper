7c30661c6957e125b28a2dbaf842858c974279e0
hamza <hamzazizkhan@gmail.com>
2025-02-09 12:39:06 +0300

    adds tables to text files

--- one_fighter.py
+++ one_fighter.py
@@ -2,31 +2,104 @@ import requests
 from bs4 import BeautifulSoup
 import os
 from main_table import tables
+import sqlite3
 
-url = "https://en.wikipedia.org/wiki/Khabib_Nurmagomedov"
-fighter_name, prof_rec_table, main_rec_table = tables(url)
+conn = sqlite3.connect('fighters.sqlite')
+cur = conn.cursor()
 
+cur.execute('SELECT firstName, lastName, html FROM visited_links')
+data = cur.fetchall()
 
-prof_rec_data = {}
-prof_rec_list = prof_rec_table.tbody.get_text().split()
-
-for i in range(len(prof_rec_list)):
-    if i%2==0 and i<6:
-        prof_rec_data[prof_rec_list[i+1]] = prof_rec_list[i]
-    if prof_rec_list[i]=='By':
-        prof_rec_data[prof_rec_list[i+1]+ 'Wins'] =  prof_rec_list[i+2]
-        prof_rec_data[prof_rec_list[i+1]+ 'Losses'] =  prof_rec_list[i+3]
-print('\n')
-print(prof_rec_data)
-
-# writing to text file
-if(os.path.exists('fighters.txt')): os.remove('fighters.txt')
-
+# for now remove txt file
+#if(os.path.exists('fighters.txt')): os.remove('fighters.txt')
 fighters = open('fighters.txt','a')
-fighters.write('firstName '+fighter_name.split()[0] + ' ')
-fighters.write('lastName '+fighter_name.split()[-1] + ' ')
+#if(os.path.exists('main_table.txt')): os.remove('main_table.txt')
+file = open('main_table.txt', 'a')
 
-for k,v in prof_rec_data.items():
-    text = k + ' '+v + ' '
-    fighters.write(text)
+for row in data:
+    print(row[0])
+    html = row[2]
+    fighter_name, prof_rec_table, main_rec_table, html_content = tables(html)
+
+    prof_rec_data = {}
+    prof_rec_list = prof_rec_table.tbody.get_text().split()
+
+    for i in range(len(prof_rec_list)):
+        if i%2==0 and i<6:
+            prof_rec_data[prof_rec_list[i+1]] = prof_rec_list[i]
+        if prof_rec_list[i]=='By':
+            prof_rec_data[prof_rec_list[i+1]+ 'Wins'] =  prof_rec_list[i+2]
+            prof_rec_data[prof_rec_list[i+1]+ 'Losses'] =  prof_rec_list[i+3]
+    print('\n')
+    print(prof_rec_data)
+
+    # writing to text file
+    
+    fighters.write('firstName '+fighter_name.split()[0] + ' ')
+    fighters.write('lastName '+fighter_name.split()[-1] + ' ')
+
+    for k,v in prof_rec_data.items():
+        text = k + ' '+v + ' '
+        fighters.write(text)
+    fighters.write('\n')
+
+    ################################ main table
+
+    print('\n')
+    print('=================')
+
+    
+    file.write(fighter_name.split()[0] + ' ' + fighter_name.split()[1]+ '// ')
+
+    rows = main_rec_table.find_all('tr')
+    qu = []
+    i=0
+    rowspan = []
+    inserts=0
+    for row in rows:
+        if i ==0:
+            i=1
+            continue
+        #print(row)
+        td = row.find_all('td')
+        text = [td_tag.text.strip() for td_tag in td]
+        
+        
+        if len(rowspan)!=0:
+            for ele in rowspan:
+                text_insert = ele[0]
+                index_insert = ele[1]
+                
+                text.insert(index_insert, text_insert)
+            if inserts==1:
+                rowspan=[]
+                inserts=0
+            else:
+                inserts-=1
+
+        else:
+            #rowspan = [(int(td_tag['rowspan'])-1, td_tag.text.strip(), i) for i, td_tag in enumerate(td) if 'rowspan' in td_tag.attrs]
+            for i, td_tag in enumerate(td):
+                if 'rowspan' in td_tag.attrs:
+                    rowspan.append((td_tag.text.strip(), i))
+                    inserts = int(td_tag['rowspan'])-1
+        if len(text)<10:
+            diff = 10-len(text)
+            for _ in range(diff):
+                text.append('Null')
+
+
+        #print(rowspan)
+
+        #print('=================')
+        
+
+        for word in text:
+            file.write(word + '// ')
+
+        #print('=================')
+    file.write('\n')
+
+
+file.close()
 fighters.close()
