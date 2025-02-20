d610587d2f57fd7de2ffaa14245114901ed87da6
hamza <hamzazizkhan@gmail.com>
2025-02-20 14:57:15 +0300

    enable delete all tables fast

--- restart_crawl.py
+++ restart_crawl.py
@@ -3,13 +3,11 @@ import sqlite3
 conn = sqlite3.connect('fighters.sqlite')
 cur = conn.cursor()
 
-profesional_record_data=False
-MMA_record=False
-qu=False
-visited_links=False
-all=True
+all=input('del all? ')
 
-if all:
+
+
+if all=='y':
     hmm = input('are you sure you want to delete all tables? y/n')
     if hmm=='y':
         cur.execute('DROP TABLE IF EXISTS profesional_record_data')
@@ -18,27 +16,32 @@ if all:
         cur.execute('DROP TABLE IF EXISTS visited_links')
         print('all tables in fighters.sqlite deleted')
     
-    
-if profesional_record_data:
-    hmm = input('are you sure you want to delete profesional_record_data? y/n')
-    if hmm=='y':
-        cur.execute('DROP TABLE IF EXISTS profesional_record_data')
-        print('deleted profesional_record_data')
-if MMA_record:
-    hmm = input('are you sure you want to delete MMA_record? y/n')
-    if hmm=='y':
-        cur.execute('DROP TABLE IF EXISTS MMA_record')
-        print('deleted MMA_record')
-if qu:
-    hmm = input('are you sure you want to delete qu? y/n')
-    if hmm=='y':
-        cur.execute('DROP TABLE IF EXISTS qu')
-        print('deleted qu')
-if visited_links:
-    hmm = input('are you sure you want to delete visited_links? y/n')
-    if hmm=='y':
-        cur.execute('DROP TABLE IF EXISTS visited_links')
-        print('deleted visited_links')
+if all=='n':
+    profesional_record_data=input('del profesional_record_data? ')
+    MMA_record=input('del MMA_record? ')
+    qu=input('del qu? ')
+    visited_links=input('del visited_links? ')
+
+    if profesional_record_data=='y':
+        hmm = input('are you sure you want to delete profesional_record_data? y/n')
+        if hmm=='y':
+            cur.execute('DROP TABLE IF EXISTS profesional_record_data')
+            print('deleted profesional_record_data')
+    if MMA_record=='y':
+        hmm = input('are you sure you want to delete MMA_record? y/n')
+        if hmm=='y':
+            cur.execute('DROP TABLE IF EXISTS MMA_record')
+            print('deleted MMA_record')
+    if qu=='y':
+        hmm = input('are you sure you want to delete qu? y/n')
+        if hmm=='y':
+            cur.execute('DROP TABLE IF EXISTS qu')
+            print('deleted qu')
+    if visited_links=='y':
+        hmm = input('are you sure you want to delete visited_links? y/n')
+        if hmm=='y':
+            cur.execute('DROP TABLE IF EXISTS visited_links')
+            print('deleted visited_links')
 
 conn.commit()
 cur.close()
